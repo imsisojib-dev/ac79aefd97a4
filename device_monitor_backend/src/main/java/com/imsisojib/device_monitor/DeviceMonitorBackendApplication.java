1e9b1246c0d792e8ce2d1cf9317405e4afe4a7b1
siraju.sojib <sirajul.sojib@shamolima.com>
2026-01-25 12:03:43 +0600

    initialized: Spring Boot project initialized using Spring Initializer website.

---
+++ device_monitor_backend/src/main/java/com/imsisojib/device_monitor/DeviceMonitorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.imsisojib.device_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceMonitorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceMonitorBackendApplication.class, args);
+	}
+
+}
---
+++ device_monitor_backend/src/test/java/com/imsisojib/device_monitor/DeviceMonitorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imsisojib.device_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceMonitorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
